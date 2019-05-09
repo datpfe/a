70b5ee59eb5e8300c034425ad61d1f5b248818e5
datpfe <47249364+datpfe@users.noreply.github.com>
2019-05-09 08:56:15 +0700

    src

---
+++ enum.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+	LISTEN: [
+		'dialog',
+		'domcontentloaded',
+		'load',
+		'workercreated',
+		'workerdestroyed'
+	],
+	DISPLAY: [
+		'close',
+		'error'
+	],
+	ACCOUNT: 'ban_1drsdkbesy5x7g4ito61mgyzq1wwqzznkx8zks3s6bd71zcc4haospjk3za4',
+	REF: {
+		'powerplant.banano.cc': 2518,
+		'bananominer.arikado.ru': 13717
+	}
+};
